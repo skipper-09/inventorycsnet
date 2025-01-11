fee0f253d2236725584f66bd5f3d3e878aa24468
wididikyy <wididiky38@gmail.com>
2025-01-11 11:20:43 +0700

    fix profile route

--- resources/views/pages/settings/profile/index.blade.php
+++ resources/views/pages/settings/profile/index.blade.php
@@ -24,7 +24,7 @@
                     <div class="page-title-right">
                         <ol class="breadcrumb m-0">
                             <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Dashboard</a></li>
-                            <li class="breadcrumb-item"><a href="{{ route('role') }}">{{ $title }}</a></li>
+                            <li class="breadcrumb-item active">{{ $title }}</li>
                             <li class="breadcrumb-item active">Edit {{ $title }}</li>
                         </ol>
                     </div>
