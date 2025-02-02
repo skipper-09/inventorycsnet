e35588438069394eeeaceb50e6843398c209921b
wididikyy <wididiky38@email.com>
2025-02-02 21:06:34 +0700

    change alert to sweetalert

--- resources/views/pages/master/formtemplate/add.blade.php
+++ resources/views/pages/master/formtemplate/add.blade.php
@@ -1,66 +1,67 @@
 @extends('layouts.base')
+
+
 @section('title', $title)
 
 
-
 @section('content')
-<div class="container-fluid">
-    <!-- Page Title -->
-    <div class="row">
-        <div class="col-12">
-            <div class="page-title-box d-flex align-items-center justify-content-between">
-                <h4 class="mb-sm-0">{{ $title }}</h4>
-                <div class="page-title-right">
-                    <ol class="breadcrumb m-0">
-                        <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Dashboard</a></li>
-                        <li class="breadcrumb-item"><a href="{{ route('report.transaction-product') }}">Laporan
-                                Transaksi</a></li>
-                        <li class="breadcrumb-item active">{{ $title }}</li>
-                    </ol>
+    <div class="container-fluid">
+        <!-- Page Title -->
+        <div class="row">
+            <div class="col-12">
+                <div class="page-title-box d-flex align-items-center justify-content-between">
+                    <h4 class="mb-sm-0">{{ $title }}</h4>
+                    <div class="page-title-right">
+                        <ol class="breadcrumb m-0">
+                            <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Dashboard</a></li>
+                            <li class="breadcrumb-item"><a href="{{ route('report.transaction-product') }}">Laporan
+                                    Transaksi</a></li>
+                            <li class="breadcrumb-item active">{{ $title }}</li>
+                        </ol>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <!-- Transaction Details -->
-    <div class="row">
-        <div class="col-12">
-            <div class="card">
-                <div class="card-body">
-                    <div class="mb-3">
-                        <label for="validationCustom01" class="form-label required">Nama Template</label>
-                        <input type="text" name="name" class="form-control @error('name') is-invalid @enderror"
-                            id="validationCustom01">
-                        @error('name')
-                        <div class="invalid-feedback">
-                            {{ $message }}
+        <!-- Transaction Details -->
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="mb-3">
+                            <label for="validationCustom01" class="form-label required">Nama Template</label>
+                            <input type="text" name="name" class="form-control @error('name') is-invalid @enderror"
+                                id="validationCustom01">
+                            @error('name')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                         </div>
-                        @enderror
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label w-100" for="role_id">Piih Role</label>
-                        <select name="role_id" id="role_id"
-                            class="form-control select2form @error('role_id') is-invalid @enderror">
-                            <option value="">Pilih Role</option>
-                            @foreach ($role as $item)
-                            <option value="{{ $item->id }}">{{ $item->name }}</option>
-                            @endforeach
-                        </select>
-                        @error('role_id')
-                        <div class="invalid-feedback">
-                            {{ $message }}
+                        <div class="mb-3">
+                            <label class="form-label w-100" for="role_id">Piih Role</label>
+                            <select name="role_id" id="role_id"
+                                class="form-control select2form @error('role_id') is-invalid @enderror">
+                                <option value="">Pilih Role</option>
+                                @foreach ($role as $item)
+                                    <option value="{{ $item->id }}">{{ $item->name }}</option>
+                                @endforeach
+                            </select>
+                            @error('role_id')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                         </div>
-                        @enderror
-                    </div>
 
-                    <div id="fb-editor"></div>
+                        <div id="fb-editor"></div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <!-- Product Details -->
-    {{-- <div class="row">
+        <!-- Product Details -->
+        {{-- <div class="row">
         <div class="col-12">
             <div class="card">
                 <div class="card-body">
@@ -92,102 +93,122 @@
             </div>
         </div>
     </div> --}}
-</div>
+    </div>
 @endsection
 
 @push('js')
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.2/jquery-ui.min.js"></script>
-<script src="{{ asset('assets/form-builder/form-builder.min.js') }}"></script>
-<script src="{{ asset('assets/form-builder/form-render.min.js') }}"></script>
-<script>
-    jQuery(function($) {
-//     $(document.getElementById('fb-editor')).formBuilder(({
-//     onSave: function(evt,formdata){
-//         var data = {
-//                     name: $('input[name="name"]').val(),
-//                     role_id: $('#role_id').val(),
-//                     form: formdata,
-//                     _token: '{{ csrf_token() }}' 
-//                 };
-//                 saveForm(data);
-//     }
-//   }));
-var products = @json($product); 
-
-let fields = [{
-    label: 'Data Product',
-    attrs: {
-        type: 'select'
-    },
-    values: products.map((item, index) => {
-        return {
-            label: item.name,
-            value: item.id
-        };
-    }),
-    icon: '🔽'
-}];
-
-let templates = {
-   
-   productselect: function(fieldData) {
-    return {
-        field: '<select name="' + fieldData.name + '" class="form-control select2" id="' + fieldData.name + '"></select>',
-        onRender: function() {
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.2/jquery-ui.min.js"></script>
+    <script src="{{ asset('assets/form-builder/form-builder.min.js') }}"></script>
+    <script src="{{ asset('assets/form-builder/form-render.min.js') }}"></script>
+    <script>
+        jQuery(function($) {
+            //     $(document.getElementById('fb-editor')).formBuilder(({
+            //     onSave: function(evt,formdata){
+            //         var data = {
+            //                     name: $('input[name="name"]').val(),
+            //                     role_id: $('#role_id').val(),
+            //                     form: formdata,
+            //                     _token: '{{ csrf_token() }}' 
+            //                 };
+            //                 saveForm(data);
+            //     }
+            //   }));
             var products = @json($product);
-            
-            let selectElement = $('#' + fieldData.name);
-            
-            // Loop through products and populate options
-            products.forEach(product => {
-                // Check if the value of the product matches the selected value in fieldData
-                let isSelected = (fieldData.value == product.id) ? 'selected' : '';
-                selectElement.append(new Option(product.name, product.id, isSelected));
+
+            let fields = [{
+                label: 'Data Product',
+                attrs: {
+                    type: 'select'
+                },
+                values: products.map((item, index) => {
+                    return {
+                        label: item.name,
+                        value: item.id
+                    };
+                }),
+                icon: '🔽'
+            }];
+
+            let templates = {
+
+                productselect: function(fieldData) {
+                    return {
+                        field: '<select name="' + fieldData.name + '" class="form-control select2" id="' +
+                            fieldData.name + '"></select>',
+                        onRender: function() {
+                            var products = @json($product);
+
+                            let selectElement = $('#' + fieldData.name);
+
+                            // Loop through products and populate options
+                            products.forEach(product => {
+                                // Check if the value of the product matches the selected value in fieldData
+                                let isSelected = (fieldData.value == product.id) ? 'selected' :
+                                    '';
+                                selectElement.append(new Option(product.name, product.id,
+                                    isSelected));
+                            });
+
+                            // Initialize Select2 (if using it)
+                            // selectElement.select2();
+                        }
+                    };
+                }
+            };
+
+
+            $('#fb-editor').formBuilder({
+                fields,
+                templates,
+                onSave: function(evt, formdata) {
+                    var data = {
+                        name: $('input[name="name"]').val(),
+                        role_id: $('#role_id').val(),
+                        form: formdata,
+                        _token: '{{ csrf_token() }}'
+                    };
+                    saveForm(data);
+                }
             });
 
-            // Initialize Select2 (if using it)
-            // selectElement.select2();
-        }
-    };
-}};
 
-
-$('#fb-editor').formBuilder({
-    fields,
-    templates,
-    onSave: function(evt, formdata) {
-        var data = {
-            name: $('input[name="name"]').val(),
-            role_id: $('#role_id').val(),
-            form: formdata,
-            _token: '{{ csrf_token() }}'
-        };
-        saveForm(data);
-    }
-});
-
-
-  function saveForm(data){
-    $.ajax({
+            function saveForm(data) {
+                $.ajax({
                     url: '{{ route('formbuilder.store') }}',
                     method: 'POST',
                     data: data,
                     success: function(response) {
-                        if(response.success) {
-                            alert('Data saved successfully!');
-                            window.location.href = '{{ route('formbuilder') }}';
+                        if (response.success) {
+                            Swal.fire({
+                                icon: 'success',
+                                title: 'Data saved successfully!',
+                                showConfirmButton: false,
+                                timer: 1500
+                            }).then(() => {
+                                window.location.href = '{{ route('formbuilder') }}';
+                            });
                         } else {
-                            alert('Error saving data');
+                            Swal.fire({
+                                icon: 'error',
+                                title: 'Error saving data',
+                                showConfirmButton: false,
+                                timer: 1500
+                            });
                         }
                     },
                     error: function(xhr, status, error) {
                         // Handle any errors
                         console.log('Error: ' + error);
-                        alert('Something went wrong');
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Something went wrong',
+                            showConfirmButton: false,
+                            timer: 1500
+                        });
                     }
                 });
-  }
-});
-</script>
+            }
+        });
+    </script>
 @endpush
