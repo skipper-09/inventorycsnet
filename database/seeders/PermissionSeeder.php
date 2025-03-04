df79d061ba67a633183ef3b3dd7ffbdb0984dd6a
wididikyy <wididiky38@email.com>
2025-03-04 09:11:27 +0700

    fix fitur customer, permission role seeder, sweetalert activity report

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -16,90 +16,63 @@ class PermissionSeeder extends Seeder
     public function run()
     {
         $arrayOfPermissionNames = [
+            // Dashboard
             'read-dashboard',
+            // Branch
             'read-branch',
             'create-branch',
             'update-branch',
             'delete-branch',
+            // ODP
             'read-odp',
             'create-odp',
             'update-odp',
             'delete-odp',
+            // Product
             'read-product',
             'create-product',
             'update-product',
             'delete-product',
+            // Unit Product
             'read-unit-product',
             'create-unit-product',
             'update-unit-product',
             'delete-unit-product',
+            // Zone
             'read-zone',
             'delete-zone',
             'read-zone-odp',
             'delete-zone-odp',
+            // Product Role
             'read-product-role',
             'create-product-role',
             'update-product-role',
             'delete-product-role',
+            // Deduction
             'read-deduction',
             'create-deduction',
             'update-deduction',
             'delete-deduction',
+            // Deduction Type
             'read-deduction-type',
             'create-deduction-type',
             'update-deduction-type',
             'delete-deduction-type',
+            // Allowance
             'read-allowance',
             'create-allowance',
             'update-allowance',
             'delete-allowance',
+            // Allowance Type
             'read-allowance-type',
             'create-allowance-type',
             'update-allowance-type',
             'delete-allowance-type',
+            // Position
             'read-position',
             'create-position',
             'update-position',
             'delete-position',
-            'read-department',
-            'create-department',
-            'update-department',
-            'delete-department',
-            // employee
-            'read-employee',
-            'create-employee',
-            'update-employee',
-            'delete-employee',
-            // salary
-            'read-salary',
-            'create-salary',
-            'update-salary',
-            'delete-salary',
-            //tasktemplate
-            'read-task-template',
-            'create-task-template',
-            'update-task-template',
-            'delete-task-template',
-            //task
-            'read-task',
-            'create-task',
-            'update-task',
-            'delete-task',
-            //detailtask
-            'read-detail-task',
-            'create-detail-task',
-            'update-detail-task',
-            'delete-detail-task',
-            //assignment
-            'read-assignment',
-            'create-assignment',
-            'update-assignment',
-            'delete-assignment',
-            //assigment Data
-            'read-assigmentdata',
-            'create-assigmentdata',
-            'update-assigmentdata',
-            'delete-assigmentdata',
             //leave
             'read-leave',
             'create-leave',
@@ -120,22 +93,70 @@ class PermissionSeeder extends Seeder
             'create-activity-report',
             'update-activity-report',
             'delete-activity-report',
-            // Transaction
+            //customer
+            'read-customer',
+            'create-customer',
+            'update-customer',
+            'delete-customer',
+            //departement
+            'read-department',
+            'create-department',
+            'update-department',
+            'delete-department',
+            //task template
+            'read-task-template',
+            'create-task-template',
+            'update-task-template',
+            'delete-task-template',
+            //task
+            'read-task',
+            'create-task',
+            'update-task',
+            'delete-task',
+            //detailtask
+            'read-detail-task',
+            'create-detail-task',
+            'update-detail-task',
+            'delete-detail-task',
+            //assigment
+            'read-assignment',
+            'create-assignment',
+            'update-assignment',
+            'delete-assignment',
+            //assigment Data
+            'read-assigmentdata',
+            'create-assigmentdata',
+            'update-assigmentdata',
+            'delete-assigmentdata',
+            // employee
+            'read-employee',
+            'create-employee',
+            'update-employee',
+            'delete-employee',
+            // salary
+            'read-salary',
+            'create-salary',
+            'update-salary',
+            'delete-salary',
+            // Transfer Product
             'read-transfer-product',
             'create-transfer-product',
             'update-transfer-product',
             'delete-transfer-product',
+            // Work Product
             'read-work-product',
             'create-work-product',
             'update-work-product',
             'delete-work-product',
+            // Transaction Product
             'read-transaction-product',
             'export-transaction-product',
-            // Setting
+            // User
             'read-user',
             'create-user',
             'update-user',
             'delete-user',
+            // Role
             'read-role',
             'create-role',
             'update-role',
@@ -144,7 +165,6 @@ class PermissionSeeder extends Seeder
             'read-activity-log',
             'delete-activity-log',
             'read-setting',
-
         ];
 
         $permissions = collect($arrayOfPermissionNames)->map(function ($permission) {
