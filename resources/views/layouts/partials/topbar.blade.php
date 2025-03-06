d2b442136673c777f17eac5e1ae18ef23050b62b
skipper-09 <dxdskipper@gmail.com>
2025-03-06 09:20:39 +0700

    add mail notification

--- resources/views/layouts/partials/topbar.blade.php
+++ resources/views/layouts/partials/topbar.blade.php
@@ -32,7 +32,7 @@
         <!-- Start menu -->
         <div class="d-flex justify-content-between menu-sm px-3 ms-auto">
             <div class="d-flex align-items-center gap-2">
-               
+
             </div>
 
             <div class="d-flex align-items-center gap-2">
@@ -47,18 +47,21 @@
 
                 <!-- Start Notification -->
                 <div class="dropdown d-inline-block">
-                    <button type="button" class="btn btn-sm top-icon" id="page-header-notifications-dropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                    <button type="button" class="btn btn-sm top-icon" id="page-header-notifications-dropdown"
+                        data-bs-toggle="dropdown" aria-expanded="false">
                         <i class="fas fa-bell align-middle"></i>
                         <span class="btn-marker"><i class="marker marker-dot text-danger"></i><span>
                     </button>
-                    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-md dropdown-menu-end p-0" aria-labelledby="page-header-notifications-dropdown">
+                    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-md dropdown-menu-end p-0"
+                        aria-labelledby="page-header-notifications-dropdown">
                         <div class="p-3 bg-info">
                             <div class="row align-items-center">
                                 <div class="col">
                                     <h6 class="text-white m-0"><i class="far fa-bell me-2"></i> Notifications </h6>
                                 </div>
                                 <div class="col-auto">
-                                    <a href="#!" class="badge bg-info-subtle text-info">{{ count($unreadNotifications) }}</a>
+                                    <a href="#!" class="badge bg-info-subtle text-info">{{ count($unreadNotifications)
+                                        }}</a>
                                 </div>
                             </div>
                         </div>
@@ -74,14 +77,15 @@
                                     <div class="flex-1">
                                         <h6 class="mb-1">{{ $notification->data['data'] }}</h6>
                                         <div class="fs-12 text-muted">
-                                            <p class="mb-0"><i class="mdi mdi-clock-outline"></i>{{ $notification->created_at->diffForHumans() }}</p>
+                                            <p class="mb-0"><i class="mdi mdi-clock-outline"></i>{{
+                                                $notification->created_at->diffForHumans() }}</p>
                                         </div>
                                     </div>
                                     <i class="mdi mdi-chevron-right align-middle ms-2"></i>
                                 </div>
                             </a>
                             @endforeach
-                            
+
                         </div>
                         <div class="p-2 border-top">
                             <div class="d-grid">
@@ -95,14 +99,18 @@
                 <!-- End Notification -->
                 <!-- Start Profile -->
                 <div class="dropdown d-inline-block">
-                    <button type="button" class="btn btn-sm top-icon p-0" id="page-header-user-dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <button type="button" class="btn btn-sm top-icon p-0" id="page-header-user-dropdown"
+                        data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         @if (Auth::user()->picture == null)
-                        <img class="rounded avatar-2xs p-0" src="{{ asset('assets/images/users/avatar-1.png') }}" alt="Header Avatar">
+                        <img class="rounded avatar-2xs p-0" src="{{ asset('assets/images/users/avatar-1.png') }}"
+                            alt="Header Avatar">
                         @else
-                        <img class="rounded avatar-2xs p-0" src="{{ asset('storage/images/user/' . Auth::user()->picture) }}" alt="Header Avatar">
+                        <img class="rounded avatar-2xs p-0"
+                            src="{{ asset('storage/images/user/' . Auth::user()->picture) }}" alt="Header Avatar">
                         @endif
                     </button>
-                    <div class="dropdown-menu dropdown-menu-md dropdown-menu-end dropdown-menu-animated overflow-hidden py-0">
+                    <div
+                        class="dropdown-menu dropdown-menu-md dropdown-menu-end dropdown-menu-animated overflow-hidden py-0">
                         <div class="card border-0">
                             <div class="card-header bg-primary rounded-0">
                                 <div class="rich-list-item w-50 p-0">
@@ -115,7 +123,8 @@
                                         <h3 class="rich-list-title text-white">{{ Auth::user()->name }}</h3>
                                         <span class="rich-list-subtitle text-white">{{ Auth::user()->email }}</span>
                                     </div>
-                                    {{-- <div class="rich-list-append"><span class="badge badge-label-light fs-6">6+</span></div> --}}
+                                    {{-- <div class="rich-list-append"><span
+                                            class="badge badge-label-light fs-6">6+</span></div> --}}
                                 </div>
                             </div>
                             <div class="card-body p-0">
@@ -132,7 +141,8 @@
                                     </div>
                                 </a>
                             </div>
-                            <div class="card-footer card-footer-bordered rounded-0"><a href="{{ route('auth.signout') }}" class="btn btn-label-danger">Sign out</a></div>
+                            <div class="card-footer card-footer-bordered rounded-0"><a
+                                    href="{{ route('auth.signout') }}" class="btn btn-label-danger">Sign out</a></div>
                         </div>
                     </div>
                 </div>
