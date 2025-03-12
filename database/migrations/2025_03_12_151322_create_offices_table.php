b651a2e39ec5f4cd0b314c0a8955348d3978eaf1
skipper-09 <dxdskipper@gmail.com>
2025-03-12 16:52:44 +0700

    add new migration

---
+++ database/migrations/2025_03_12_151322_create_offices_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('offices', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('company_id');
+            $table->string('name');
+            $table->string('lat');
+            $table->string('long');
+            $table->integer('radius');
+            $table->longText('address');
+            $table->foreign('company_id')->references('id')->on('companies')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('offices');
+    }
+};
